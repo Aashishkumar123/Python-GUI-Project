800d7dad3a4c0f5e62aed2e11baee4cfd16ef5b0
Aashish Kumar <39042206+Aashishkumar123@users.noreply.github.com>
2020-09-18 04:27:45 -0700

    Update piano.py

--- piano/piano.py
+++ piano/piano.py
@@ -3,10 +3,6 @@ import tkinter as tk
 import winsound
 from PIL import ImageTk, Image
 from playsound import playsound
-import multiprocessing
-import time
-import sounddevice as sd
-from scipy.io.wavfile import write
 
 class piano():
 
